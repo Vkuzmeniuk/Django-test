1e58c50e55d8d8bcdcc0faf5fe196189eaf5fbc5
Vkuzmeniuk <77533405+Vkuzmeniuk@users.noreply.github.com>
2021-04-27 01:08:54 +0300

    Пройденная первая часть( до 71 страницы)

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -13,3 +13,17 @@ def by_rubric(request, rubric_id):
     context= {'bbs':bbs,'rubrics':rubrics,
               'current_rubric':current_rubric}
     return render(request,'bboard/by_rubric.html',context)
+
+from django.views.generic.edit import CreateView
+from .forms import BbForm
+from django.urls import reverse_lazy
+
+class BbCreateView (CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+    
+    def gen_context_data (self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
